fe4b3ed30bbe8c9c90cd490e9259bbc72bb0638c
bachardaowd <bachar.daowd@gmail.com>
2023-07-06 21:45:18 +0200

    Put method

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -14,13 +14,20 @@ public class Basics {
         //Then - validate the response
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        String response = given().relaxedHTTPSValidation().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+        String response = given().relaxedHTTPSValidation().log().all().queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json")
                 .body(payload.AddPlace()).when().post("maps/api/place/add/json")
                 .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
                 .header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 
         JsonPath js = new JsonPath(response);// for parsing json
         String placeId = js.getString("place_id");
+
+        given().relaxedHTTPSValidation().log().all().queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json")
+                .body("\"{\\n\\\"place_id\\\":\\\""+ placeId +",\\n\\\"address\\\":\\\"70 Summer walking, USA\\\",\\n\\\"key\\\":\\\"qaclick123\\\"\\n}\"")
+                .when().put("maps/api/place/update/json")
+                .then().assertThat().log().all().statusCode(200);
 
     }
 }
