9473813c22506b7af47e8ba7ccad08b5a01a40f5
bachardaowd <bachar.daowd@gmail.com>
2023-07-06 16:45:19 +0200

    Parsing the json response body using jsonpath class

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,5 +1,6 @@
 import files.payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -7,15 +8,19 @@ import static org.hamcrest.Matchers.*;
 public class Basics {
     public static void main(String[] args) {
         // validate if Add Place API is working as expected
+        // Add place -> Update Place with New Address -> Get Place to validate if New address is present in response
         //given - all input details
         // when - Submit the API -resource, http method
         //Then - validate the response
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().relaxedHTTPSValidation().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+        String response = given().relaxedHTTPSValidation().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
                 .body(payload.AddPlace()).when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)");
+                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 
-        // Add place -> Update Place with New Address -> Get Place to validate if New address is present in response
+        JsonPath js = new JsonPath(response);// for parsing json
+        String placeId = js.getString("place_id");
+
     }
 }
