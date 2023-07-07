eb742d60f5396fba4510e9bf5a3c6ac947b2d25d
Bachar Daowd <bachar.daowd@gmail.com>
2023-07-07 08:35:37 +0200

    Get updated place

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,6 +1,7 @@
-import files.payload;
+import files.*;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -13,21 +14,42 @@ public class Basics {
         // when - Submit the API -resource, http method
         //Then - validate the response
 
+        //Base URI
         RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+        //Post Scenario
         String response = given().relaxedHTTPSValidation().log().all().queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
                 .body(payload.AddPlace()).when().post("maps/api/place/add/json")
                 .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
                 .header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 
-        JsonPath js = new JsonPath(response);// for parsing json
+        //extracting place id
+        JsonPath js = reUsableMethods.rawToJson(response);// for parsing json
         String placeId = js.getString("place_id");
 
+        String updateNewAddress = "70 Summer, Africa";
+        //Put Scenario
         given().relaxedHTTPSValidation().log().all().queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
-                .body("\"{\\n\\\"place_id\\\":\\\""+ placeId +",\\n\\\"address\\\":\\\"70 Summer walking, USA\\\",\\n\\\"key\\\":\\\"qaclick123\\\"\\n}\"")
+                .body("{\n" +
+                        "\"place_id\":\"" +placeId + "\",\n" +
+                        "\"address\":\"" + updateNewAddress +"\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}")
                 .when().put("maps/api/place/update/json")
-                .then().assertThat().log().all().statusCode(200);
+                .then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+        //Get Scenario
+        String getPlace = given().relaxedHTTPSValidation().log().all().queryParam("key", "qaclick123")
+                .queryParam("place_id", placeId)
+                .when().get("maps/api/place/get/json")
+                .then().assertThat().log().all().statusCode(200).body("address", equalTo(updateNewAddress)).extract().response().asString();
+        JsonPath getPlaceJson = reUsableMethods.rawToJson(getPlace);
+        String getUpdatedAddress = getPlaceJson.getString("address");
+        //Junit and Testng
+        Assert.assertEquals(updateNewAddress, getUpdatedAddress);
+
 
     }
 }
